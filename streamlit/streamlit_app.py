bfdacba64f946de2d07497a6f7a4118b63ee8097
Miguel Bueno <bueno.reyes.miguel@gmail.com>
2024-03-27 15:55:58 +0100

    Added logging for troubleshooting purposes

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,6 +1,18 @@
+import os
+import sys
 import streamlit as st
 import requests
+import logging
+
+# Add parent directory of predict.py to Python path
+predict_module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(predict_module_path)
+
 from predict import Item
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 st.title('House Price Prediction')
 
@@ -23,11 +35,13 @@ item = Item(
 
 if st.button('Predict'):
     # Make a POST request to your FastAPI backend
-    # response = requests.post('http://localhost:8000/predict/', json=item.dict())
-    response = requests.post('https://immo-eliza-deployment-h5ai.onrender.com', json=item.dict())
+    response = requests.post('http://localhost:8000/predict/', json=item.dict())
 
     if response.status_code == 200:
         prediction = response.json()['prediction']
-        st.success(f'Prediction: {prediction}')
+        formatted_prediction = "{:,.2f} €".format(prediction)  # Format prediction with comma and 2 decimals
+        st.success(f'Prediction: {formatted_prediction}')
+        logger.info('Prediction successful')
     else:
         st.error('Prediction failed. Please try again.')
+        logger.error('Prediction failed')
